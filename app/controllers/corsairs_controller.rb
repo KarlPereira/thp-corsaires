1fdac3ec6c94825125511b441f136059f03991be
KarlPereira <karlpereira14@gmail.com>
2017-07-30 19:05:45 +0200

    commit avec début de flash me semble

--- app/controllers/corsairs_controller.rb
+++ app/controllers/corsairs_controller.rb
@@ -3,6 +3,10 @@ class CorsairsController < ApplicationController
     @corsairs = Corsair.new
   end
 
+  def create
+corsaire = Corsair.create(post_params)
+end
+
   def show
     @corsairs = Corsair.find(params[:id])
   end
@@ -11,10 +15,31 @@ class CorsairsController < ApplicationController
     @corsairs = Corsair.find(params[:id])
   end
 
-  def delete
+def update
+  @corsairs = Corsair.find(params[:id])
+  if
+  @corsairs.update(post_params)
+  flash[:success] = "Welcome to the Sample App!"
+  redirect_to corsair_path
+else
+  render 'new'
+end
+end
+
+  def destroy
+    @corsairs = Corsair.find(params[:id])
+    @corsairs.destroy
+    redirect_to corsairs_path
+
   end
 
   def index
     @corsairs = Corsair.all
+  end
+
+private
+
+  def post_params
+  params.require(:corsair).permit(:first_name, :age, :slack_handle, :github_handle, :bio)
   end
 end
